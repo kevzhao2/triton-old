626da4b479984d27b89ee1d22352be0251c93f74
Kevin Zhao <kzhao1729@gmail.com>
2020-07-08 23:17:06 -0700

    Beginning commit.

---
+++ tests/Triton.Tests/Interop/LuaCApiTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace Triton.Interop
+{
+    public class LuaCApiTests
+    {
+        [Fact]
+        public void luaL_newstate()
+        {
+            var state = LuaCApi.luaL_newstate();
+
+            Assert.NotEqual(IntPtr.Zero, state);
+        }
+    }
+}
